4aed80a293464dd0777225324d81fc1a60596605
Tushar Choudhari <choudharit23@gmail.com>
2020-01-01 15:09:30 +0530

    Adding Files

---
+++ script.js
@@ -0,0 +1,36 @@
+const container = document.querySelector('.container');
+const wrap = container.querySelector('.wrapper');
+const number = 60;
+const radius = 50;
+const pi = Math.PI;
+
+function toRadians(a) {
+  return (a*pi/180);
+}
+
+
+function createKnot(radius, number){
+
+
+  let baseAngle = 360/number;
+  baseAngle = toRadians(baseAngle);
+
+
+  for (let i = 0; i < number; i++) {
+    let currentAngle = baseAngle*i;
+
+    let x = radius*(Math.sin(currentAngle) + 2*Math.sin(2*currentAngle));
+    let y = radius*(Math.cos(currentAngle) - 2*Math.cos(2*currentAngle));
+    let z = -radius*(Math.sin(3*currentAngle));
+
+    let node = document.createElement('div');
+    node.className = 'point';
+    node.style.transform = `translate3d(${x}px, ${y}px, ${z}px)`;
+    wrap.appendChild(node);
+  }
+
+}
+
+
+
+createKnot(50,60);
